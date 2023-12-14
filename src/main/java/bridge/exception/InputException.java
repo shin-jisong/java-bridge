d6514d8f94f1a2cea7a60cc042a99c76de14bf3e
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2023-12-15 00:04:06 +0900

    feat: add invalid moving to input exception

--- src/main/java/bridge/exception/InputException.java
+++ src/main/java/bridge/exception/InputException.java
@@ -2,7 +2,8 @@ package bridge.exception;
 
 public enum InputException {
 
-    INVALID_SIZE("다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+    INVALID_SIZE("다리 길이는 3부터 20 사이의 숫자여야 합니다."),
+    INVALID_MOVING("이동할 칸은 U 또는 D여야 합니다.");
 
     private final String exceptionMessage;
 
