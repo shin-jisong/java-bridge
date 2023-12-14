a9d4f1eb3e815acaa30e7d14015ebb397f0cb194
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2023-12-15 00:00:55 +0900

    feat: add read moving to input view

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -6,6 +6,7 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
 
     private final static String READ_BRIDGE_SIZE = "다리의 길이를 입력해주세요.";
+    private final static String READ_MOVING  ="이동할 칸을 선택해주세요. (위: U, 아래: D)";
 
     public int readBridgeSize() {
         System.out.println(READ_BRIDGE_SIZE);
@@ -15,7 +16,9 @@ public class InputView {
     }
 
     public String readMoving() {
-        return null;
+        System.out.println(READ_MOVING);
+        String moving = Console.readLine();
+        return moving;
     }
 
     public String readGameCommand() {
