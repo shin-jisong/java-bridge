dac1c018ab541d55ad560d7030758e586cb3511b
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2023-12-14 22:23:25 +0900

    feat: add is movable function to bridge model

--- src/main/java/bridge/model/Bridge.java
+++ src/main/java/bridge/model/Bridge.java
@@ -1,11 +1,16 @@
 package bridge.model;
 
 import java.util.List;
+import java.util.Objects;
 
 public class Bridge {
     private final List<String> bridge;
 
     public Bridge(List<String> bridge) {
         this.bridge = bridge;
+    }
+
+    public boolean isMovable(int order, String moving) {
+        return Objects.equals(bridge.get(order), moving);
     }
 }
