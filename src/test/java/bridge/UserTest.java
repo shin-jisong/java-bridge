4f38a7ff9b1efd6cc150dd910beb9a19802e3028
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2023-12-14 23:09:00 +0900

    test: check is finish to user test

--- src/test/java/bridge/UserTest.java
+++ src/test/java/bridge/UserTest.java
@@ -49,4 +49,30 @@ public class UserTest {
         }, 1, 0, 1);
 
     }
+
+    @DisplayName("다리 끝임을 테스트한다")
+    @Test
+    void checkIsFinishTrue() {
+        assertRandomNumberInRangeTest(() -> {
+            BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+            Bridge bridge = new Bridge(bridgeMaker.makeBridge(3));
+            User user = new User(bridge);
+            user.canMoving("D");
+            user.canMoving("U");
+            user.canMoving("D");
+            assertThat(user.isFinish()).isTrue();
+        }, 1, 0, 1);
+    }
+
+    @DisplayName("다리 끝이 아님을 테스트한다")
+    @Test
+    void checkIsFinishFalse() {
+        assertRandomNumberInRangeTest(() -> {
+            BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+            Bridge bridge = new Bridge(bridgeMaker.makeBridge(3));
+            User user = new User(bridge);
+            user.canMoving("D");
+            assertThat(user.isFinish()).isFalse();
+        }, 1, 0, 1);
+    }
 }
