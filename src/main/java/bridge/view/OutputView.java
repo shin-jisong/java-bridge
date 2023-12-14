a5200c942edcd3939ad7dd7b61a12539e4855ff7
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2023-12-15 00:58:24 +0900

    feat: add print map to output view

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,16 +1,40 @@
 package bridge.view;
 
-/**
- * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
- */
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class OutputView {
 
-    /**
-     * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void printMap() {
+    public void printMap(List<String> bridge, boolean move) {
+        char[][] map = makeMap(bridge);
+        if (move) {
+            mapAddX(bridge.size() - 1, map);
+        }
+        String mapBridge = IntStream.range(0, map[0].length)
+                .mapToObj(i -> "[" + map[0][i] + "]\n[" + map[1][i] + "]\n")
+                .collect(Collectors.joining(" | "));
+        System.out.println(mapBridge);
+    }
+
+    private void mapAddX(int lastIdx, char[][] map) {
+        if (map[0][lastIdx] == 'O') {
+            map[0][lastIdx] = 'X';
+            return;
+        }
+        map[1][lastIdx] = 'X';
+    }
+
+    private char[][] makeMap(List<String> bridge) {
+        char[][] map = new char[2][bridge.size()];
+        for (int i = 0; i < bridge.size(); i++) {
+            if (bridge.get(i).charAt(0) == 'U') {
+                map[0][i] = 'O';
+                continue;
+            }
+            map[1][i] = 'O';
+        }
+        return map;
     }
 
     /**
