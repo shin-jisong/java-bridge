68b39cb3e1d1d5c06c330a01bda60d753557fe09
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2023-12-15 00:06:15 +0900

    feat: add read game command to input view

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -6,7 +6,8 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
 
     private final static String READ_BRIDGE_SIZE = "다리의 길이를 입력해주세요.";
-    private final static String READ_MOVING  ="이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    private final static String READ_MOVING = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    private final static String READ_GAME_COMMAND = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
 
     public int readBridgeSize() {
         System.out.println(READ_BRIDGE_SIZE);
@@ -23,7 +24,9 @@ public class InputView {
     }
 
     public String readGameCommand() {
-        return null;
+        System.out.println(READ_GAME_COMMAND);
+        String gameCommand = Console.readLine();
+        return gameCommand;
     }
 
     private void validateMoving(String str) {
