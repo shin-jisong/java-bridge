805a50a9198d2a931804dfad6e591982c5fd515d
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2023-12-15 01:15:45 +0900

    feat: add print error to output view

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -10,16 +10,21 @@ public class OutputView {
     private final static String GAME_RESULT = "최종 게임 결과";
     private final static String GAME_SUCCESS = "게임 성공 여부: ";
     private final static String GAME_TRIAL = "총 시도한 횟수: ";
+    private final static String PRINT_ERROR = "[ERROR] ";
 
     public void printGameStart() {
         System.out.println(GAME_START);
     }
 
-    public void printGameResult(List<String> bridge, boolean move, int trial) {
+    public void printResult(List<String> bridge, boolean move, int trial) {
         System.out.println(GAME_RESULT);
         printMap(bridge, move);
         printSuccess(move);
         printTrial(trial);
+    }
+
+    public void printError(String errMsg) {
+        System.out.println(PRINT_ERROR + errMsg);
     }
 
     private void printTrial(int trial) {
@@ -63,13 +68,5 @@ public class OutputView {
             map[1][i] = 'O';
         }
         return map;
-    }
-
-    /**
-     * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void printResult() {
     }
 }
