9ea74b8c2e4101bc0024f3be88ef5f0f2a0c7d7c
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2023-12-14 22:41:15 +0900

    feat: add constructor to user model

--- src/main/java/bridge/model/User.java
+++ src/main/java/bridge/model/User.java
@@ -1,4 +1,15 @@
 package bridge.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
+
+    private List<String> userMove;
+    private Bridge bridge;
+
+    public User(Bridge bridge) {
+        this.userMove = new ArrayList<>();
+        this.bridge = bridge;
+    }
 }
