3116101ad8ddbb0c291a37f1318d12aa07a99892
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2023-12-14 22:52:36 +0900

    feat: add validate moving function to user model

--- src/main/java/bridge/model/User.java
+++ src/main/java/bridge/model/User.java
@@ -1,5 +1,6 @@
 package bridge.model;
 
+import bridge.exception.UserException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,4 +13,12 @@ public class User {
         this.userMove = new ArrayList<>();
         this.bridge = bridge;
     }
+
+    private void validateMoving(String moving) {
+        if (!moving.equals("U") && !moving.equals("D")) {
+            throw new IllegalArgumentException(UserException.INVALID_STRING.getExceptionMessage());
+        }
+    }
+
+
 }
