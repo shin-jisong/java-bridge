30831b07e8c993242767e578e1a0b300daab2ea0
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2023-12-15 01:17:37 +0900

    feat: add constructor to bridge controller

--- src/main/java/bridge/controller/BridgeController.java
+++ src/main/java/bridge/controller/BridgeController.java
@@ -1,4 +1,17 @@
 package bridge.controller;
 
+import bridge.model.BridgeGame;
+import bridge.view.InputView;
+import bridge.view.OutputView;
+
 public class BridgeController {
+    private final InputView inputView;
+    private final OutputView outputView;
+    private BridgeGame bridgeGame;
+
+
+    public BridgeController() {
+        this.inputView = new InputView();
+        this.outputView = new OutputView();
+    }
 }
